fc28b667535e6bad1619ed0f68de18463ba10276
AnaVoinea <92676726+Moozey@users.noreply.github.com>
2022-03-06 11:16:59 +0200

    Am facut fct merge

--- tema_lab1.cpp
+++ tema_lab1.cpp
@@ -1,2 +1,36 @@
 #include <iostream>
 using namespace std;
+
+void merge (int v[], int s, int m, int d)
+{
+    int i=s;
+    int j=m+1;
+    int k=s;
+    int a[100];
+    while(i<=m && j<=d)
+    {
+        if(v[i]<=v[j]) {
+            a[k] = v[i];
+            i++;
+            k++;
+        }
+        else {
+            a[k] = v[j];
+            j++;
+            k++;
+        }
+    }
+    while (i<=m) {
+        a[k] = v[i];
+        i++;
+        k++;
+    }
+    while (j<=d) {
+        a[k] = v[j];
+        j++;
+        k++;
+    }
+
+    for(int p=s; p<=d; p++)
+        v[p]=a[p];
+}
