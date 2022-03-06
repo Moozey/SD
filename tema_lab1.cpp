6e47fd4928304569969bf39a61c507f626b10983
AnaVoinea <92676726+Moozey@users.noreply.github.com>
2022-03-06 13:20:02 +0200

    am mai facut ceva-radix sort

--- tema_lab1.cpp
+++ tema_lab1.cpp
@@ -44,6 +44,40 @@ void merge_sort (int v[], int s, int d)
         merge(v,s,m,d);
     }
 }
+
+void count_sort(int v[],int dim, int d)
+{
+    int fr[10]={0};
+    int final[dim];
+
+    for(int i=0; i<dim; i++)
+        fr[(v[i]/d)%10]++;
+
+    for(int i=1; i<10; i++)
+        fr[i]=fr[i]+fr[i-1];
+
+    for(int i=dim-1; i>=0; i--)
+    {
+        final[fr[(v[i]/d)%10]-1]=v[i];
+        fr[(v[i]/d)%10]--;
+    }
+    for(int i=0; i<dim; i++)
+        v[i]=final[i];
+}
+int maxim (int v[],int m)
+{ 
+    int max=v[0];
+    for(int i=1; i<=m; i++)
+        if(max < v[i])
+            max=v[i];
+    return max;
+}
+void radix_sort(int v[], int m)
+{
+    int max=maxim(v,m);
+    for(int d=1; max/d>0; d=d*10)
+        count_sort(v,m,d);
+}
 int main() {
     int n, v[100];
     cout<<"n=";
@@ -51,7 +85,8 @@ int main() {
     cout<<"citire vector: ";
     for(int i=0; i<=n-1; i++)
         cin>>v[i];
-    merge_sort(v,0, n-1);
+    // merge_sort(v,0, n-1);
+    radix_sort(v, n);
     for (int i = 0; i <= n-1; i++)
         cout << v[i] <<" ";
     return 0;
